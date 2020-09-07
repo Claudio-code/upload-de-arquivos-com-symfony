3875a1e46aa815e5d5506b51bb42cb5207887829
claudio-code <suporte2424@outlook.com>
2020-09-07 02:46:28 -0300

    finalizado a criação do produto

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/product", name="products_")
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/", name="create", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function crete(Request $request): JsonResponse
+    {
+        $data = $request->request->all();
+
+        $product = new Product();
+        $product->setName($data['name']);
+        $product->setDescription($data['description']);
+        $product->setContent($data['content']);
+        $product->setPrice(intval($data['price']));
+        $product->setSlug($data['slug']);
+        $product->setIsActive(true);
+        $product->setCreatedAt(new \DateTime("now", new \DateTimeZone('America/Sao_Paulo')));
+        $product->setUpdatedAt(new \DateTime("now", new \DateTimeZone('America/Sao_Paulo')));
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($product);
+        $manager->flush();
+
+        return $this->json([
+            'message' => 'Produto cadastrado',
+            $product
+        ]);
+    }
+
+    /**
+     * @Route("/", name="index", methods={"GET"})
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ProductController.php',
+        ]);
+    }
+}
