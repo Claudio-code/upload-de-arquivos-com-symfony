5269c9eb196118adf099da71d198e8cfadce33a8
claudio-code <suporte2424@outlook.com>
2020-09-07 03:10:11 -0300

    adicionado tratamento para a exeção do produto

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Exception\ProductException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,29 +18,40 @@ class ProductController extends AbstractController
      * @Route("/", name="create", methods={"POST"})
      * @param Request $request
      * @return JsonResponse
+     * @throws \Exception
      */
-    public function crete(Request $request): JsonResponse
+    public function create(Request $request): JsonResponse
     {
         $data = $request->request->all();
 
-        $product = new Product();
-        $product->setName($data['name']);
-        $product->setDescription($data['description']);
-        $product->setContent($data['content']);
-        $product->setPrice(intval($data['price']));
-        $product->setSlug($data['slug']);
-        $product->setIsActive(true);
-        $product->setCreatedAt(new \DateTime("now", new \DateTimeZone('America/Sao_Paulo')));
-        $product->setUpdatedAt(new \DateTime("now", new \DateTimeZone('America/Sao_Paulo')));
+        try {
+            $product = new Product();
+            $product->setName($data['name']);
+            $product->setDescription($data['description']);
+            $product->setContent($data['content']);
+            $product->setPrice(intval($data['price']));
+            $product->setSlug($data['slug']);
+            $product->setIsActive(true);
+            $product->setCreatedAt(
+                new \DateTime("now", new \DateTimeZone('America/Sao_Paulo'))
+            );
+            $product->setUpdatedAt(
+                new \DateTime("now", new \DateTimeZone('America/Sao_Paulo'))
+            );
 
-        $manager = $this->getDoctrine()->getManager();
-        $manager->persist($product);
-        $manager->flush();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($product);
+            $manager->flush();
 
-        return $this->json([
-            'message' => 'Produto cadastrado',
-            $product
-        ]);
+            return $this->json([
+                'message' => 'Produto cadastrado',
+                'product' => $product
+            ]);
+        } catch (ProductException $productException) {
+            $this->json([
+                'error' => $productException
+            ]);
+        }
     }
 
     /**
