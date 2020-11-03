a0714e62a2d950450f7f322a1c5eff892195c638
claudio-code <suporte2424@outlook.com>
2020-11-03 01:31:28 -0300

    create upload files

--- src/Controller/ProductPhotoController.php
+++ src/Controller/ProductPhotoController.php
@@ -2,24 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/product/photo", name="product_photo_")
+ * @Route("/product", name="product_photo_")
  */
 class ProductPhotoController extends AbstractController
 {
-    /**
-     * @Route("/", name="create", methods={"POST"})
-     */
-    public function create(Request $request): JsonResponse
+	/**
+	 * @Route("/{productId}/photo", name="index", methods={"GET"})
+	 * @param Product $product
+	 * @return JsonResponse
+	 */
+    public function index(Product $product): JsonResponse
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductPhotoController.php',
+            'message' => 'Welcome to your new controller!'
         ]);
     }
+
+	/**
+	 * @Route("/photos", name="create", methods={"POST"})
+	 * @param Request $request
+	 * @return JsonResponse
+	 */
+    public function create(Request $request): JsonResponse
+	{
+		$photos = $request->files->get('photos', []);
+		foreach ($photos as $photo) {
+			if (!$photo instanceof UploadedFile) {
+				continue;
+			}
+
+			$imageName = sha1($photo->getClientOriginalName()) . uniqid() . '.' . $photo->guessExtension();
+			$photo->move($this->getParameter('upload_dir'), $imageName);
+		}
+		
+		return $this->json([
+			'message' => 'upload sucess.',
+		]);		
+	}
+
+	/**
+	 * @Route("/{productId}/photo/{photoId}", name="remove", methods={"DELETE"})
+	 * @param Product $product
+	 * @return JsonResponse
+	 */
+	public function remove(Product $product): JsonResponse
+	{
+		return $this->json([
+			'message' => 'Welcome to your new controller!'
+		]);
+	}
 }
